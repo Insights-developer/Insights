d5ea2429cc1e76223f897eff3e6f1ce956daaa73
Insights-developer <developer@lotteryanalytics.app>
2025-07-26 15:57:33 +0000

    Updated DebugInfoProvider to use StackProvider

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StackAuthProvider from "@/components/StackAuthProvider";
+import { DebugInfoProvider } from "@/components/DebugInfoContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StackAuthProvider>
-          {children}
-        </StackAuthProvider>
+        <DebugInfoProvider>
+          <StackAuthProvider>
+            {children}
+          </StackAuthProvider>
+        </DebugInfoProvider>
       </body>
     </html>
   );

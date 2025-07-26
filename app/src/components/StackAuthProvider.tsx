f5e3f113d76964768ec80d6acd59b66b3cf8c83c
Insights-developer <developer@lotteryanalytics.app>
2025-07-26 15:48:58 +0000

    Updated Stack Authentication Provider

--- app/src/components/StackAuthProvider.tsx
+++ app/src/components/StackAuthProvider.tsx
@@ -2,14 +2,11 @@
 
 import { StackProvider } from "@stackframe/stack";
 import { stackApp } from "@/lib/stack-client";
-import { DebugInfoProvider } from "./DebugInfoContext";
 
 export default function StackAuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <StackProvider app={stackApp}>
-      <DebugInfoProvider>
-        {children}
-      </DebugInfoProvider>
+      {children}
     </StackProvider>
   );
 }
